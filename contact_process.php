071c598f2ee3186bf04971547e99d7c8d9e682ce
Eulogio Castro <eulogio.castro14@gmail.com>
2019-11-20 15:55:20 -0800

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,4 +1,4 @@
-<?php
+<?/*php
 
     $to = "rockybd1995@gmail.com";
     $from = $_REQUEST['email'];
@@ -33,5 +33,25 @@
 	$body .= "</body></html>";
 
     $send = mail($to, $subject, $body, $headers);
+
+*/
+$to = 'demo@site.com'; // replace this mail with yours
+$firstname = $_POST["fname"];
+$email= $_POST["email"];
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$message ='<table style="width:100%">
+        <tbody><tr>
+                <td>'.$firstname.'  '.$laststname.'</td>
+        </tr>
+        <tr><td>Email: '.$email.'</td></tr>
+</tbody></table>';
+if (@mail($to, $email, $message, $headers))
+{
+    echo 'The message has been sent.';
+}else{
+    echo 'failed';
+}
 
 ?>
