3ef26f2bd0463d9e644e2172e39c4f2001f8f796
Eulogio Castro <eulogio.castro14@gmail.com>
2019-11-20 17:10:51 -0800

    Update SURVIVE logo/Inferno Viewport

--- Inferno/offline.js
+++ Inferno/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1574282956,
+	"version": 1574298613,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
--- SURVIVEHTML/offline.js
+++ SURVIVEHTML/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1574295788,
+	"version": 1574298360,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
