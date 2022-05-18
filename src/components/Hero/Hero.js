3e393a8ad620bc2b31cc3d2b5ec92366488c2514
Eulogio Castro <eulogio.castro14@gmail.com>
2022-05-18 12:26:37 -0700

    Update Hero.js
    Updated Hero Text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
           _Software Developer_
         </SectionTitle>
         <SectionText>
-        Motivated Computer Science graduate from California State University - Dominguez Hills with academic and personal experience in Software Development in multiple disciplines. Seeking a position to grow as a software developer alongside a great team.</SectionText>
+        Motivated early career Software Developer and Computer Science graduate from California State University - Dominguez Hills with experience in Software Development in various disciplines. Seeking a position to grow as a software developer alongside a great team.</SectionText>
       </LeftSection>
     </Section>
   </>
